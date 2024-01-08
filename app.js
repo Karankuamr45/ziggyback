b7021362d1f428580742d8d6345f36211b87df31
karan <karankumar46226@gmail.com>
2024-01-08 21:53:36 +0530

    atlas2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 import path from 'path';
 const app=express();
 const port=process.env.PORT||5000;
-const DATABASE_URL=process.env.DATABASE_URL||"mongodb+srv://karan:karan12712@cluster0.z9qdea3.mongodb.net/";
+const DATABASE_URL=process.env.DATABASE_URL||"mongodb://atlas-sql-659bd9e5cf99b310d59d471d-pof75.a.query.mongodb.net/ziggyVite?ssl=true&authSource=admin";
 
 connectdb(DATABASE_URL)
 
